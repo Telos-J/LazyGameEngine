684779d3af664cde4b0123ac715079c4cdfefebb
jiseungjung <roadrunner23@yonsei.ac.kr>
2020-10-29 18:00:01 +0900

    add realtime animation

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
-const img = new Image()
-document.querySelector('.sprite').addEventListener('change', function() {
+const img = new Image();
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+let col = 1;
+let row = 1;
+let step = 0;
+
+document.querySelector('.sprite').addEventListener('change', function () {
     if (this.files && this.files[0]) {
         img.src = URL.createObjectURL(this.files[0]);
         img.onload = imageIsLoaded;
     }
 });
 
-const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
-let col = 1
-let row = 1
+document.querySelector('.col').addEventListener('change', function () {
+    col = document.querySelector('.col').value;
+    step = 0;
+});
 
-function imageIsLoaded() { 
-   start()
+document.querySelector('.row').addEventListener('change', function () {
+    row = document.querySelector('.row').value;
+    step = 0;
+});
+
+function imageIsLoaded() {
+    start();
 }
 
 function update() {
-   col = document.querySelector('.col').value
-   row = document.querySelector('.row').value
+    step += img.width / col;
+    if (step >= img.width) step = 0;
 }
- 
+
 function render() {
-    context.fillStyle = "white"
-    context.fillRect(0,0,canvas.width, canvas.height)
-    context.drawImage(img,0,0,img.width/col,img.height/row,0,0,canvas.width,canvas.height)
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(
+        img,
+        step,
+        0,
+        img.width / col,
+        img.height / row,
+        0,
+        0,
+        canvas.height,
+        canvas.height
+    );
 }
 
 function start() {
-    update()
-    render()
-    requestAnimationFrame(start)
+    update();
+    render();
+    requestAnimationFrame(start);
 }
